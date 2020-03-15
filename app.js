35d9abaa29b8474e8b966ce741ad1582b442ccda
Joseph Bruno <brunojoey12@gmail.com>
2020-03-15 15:39:51 -0500

    Set up the Engineer.js file to work with a new employee who is console logged.

--- app.js
+++ app.js
@@ -4,98 +4,127 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
+
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
-const engineerQuestions = [
-    {
-        type: "input",
-        message: "Please enter your name.",
-        name: "name"
-    },
-    {
-        type: "input",
-        message: "Please enter your work ID.",
-        name: "id"
-    },
-    {
-        type: "input",
-        message: "Please enter your work email.",
-        name: "email"
-    },
-    {
-        type: "input",
-        message: "Please enter your work GitHub.",
-        name: "github"
-    }
-];
+function startApp() {
+    const questions = [
+        {
+            type: "confirm",
+            message: "Would you like to create a new employee?",
+            name: "newEmployee",
+            default: true
+        },
+        {
+            type: "list",
+            message: "Which employee would we like to create?",
+            name: "employeeType",
+            choices: [
+                { name: "Intern", checked: false },
+                { name: "Engineer", checked: false },
+                { name: "Manager", checked: false }
+            ]
+        },
+        {
+            type: "confirm",
+            message: "Would you liek to create another employee?",
+            name: "anotherEmployee",
+            default: false
+        }
+    ];
+    const engineerQuestions = [
+        {
+            type: "input",
+            message: "Please enter your name.",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Please enter your work ID.",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Please enter your work email.",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "Please enter your work GitHub.",
+            name: "github"
+        }
+    ];
 
-const managerQuestions = [
-    {
-        type: "input",
-        message: "Please enter your name.",
-        name: "name"
-    },
-    {
-        type: "input",
-        message: "Please enter your work ID.",
-        name: "id"
-    },
-    {
-        type: "input",
-        message: "Please enter your work email.",
-        name: "email"
-    },
-    {
-        type: "input",
-        message: "Please enter your manager office number.",
-        name: "office"
-    }
-];
+    const managerQuestions = [
+        {
+            type: "input",
+            message: "Please enter your name.",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Please enter your work ID.",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Please enter your work email.",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "Please enter your manager office number.",
+            name: "office"
+        }
+    ];
 
-const internQuestions = [
-    {
-        type: "input",
-        message: "Please enter your name.",
-        name: "name"
-    },
-    {
-        type: "input",
-        message: "Please enter your work ID.",
-        name: "id"
-    },
-    {
-        type: "input",
-        message: "Please enter your work email.",
-        name: "email"
-    },
-    {
-        type: "input",
-        message: "Please enter the school you are attending.",
-        name: "school"
-    }
-]
-​
-​
+    const internQuestions = [
+        {
+            type: "input",
+            message: "Please enter your name.",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Please enter your work ID.",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Please enter your work email.",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "Please enter the school you are attending.",
+            name: "school"
+        }
+    ];
+};
+startApp();
+
+
+const employee = [];
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-​
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-​
+
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-​
+
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-​
+
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an 
