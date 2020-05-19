e09b46c1204f427bb1f79d94e48c585d4a08468f
Joey Bruno <brunojoey12@gmail.com>
2020-05-18 22:20:45 -0500

    Added some semicolons.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
@@ -146,9 +146,8 @@ async function startApp() {
     fs.writeFile(outputPath, teamHTML, "utf8", function (err) {
         if (err) {
             return console.log("Something went wrong.");
-        }
+        };
     });
-
 };
 
 startApp();
