7c015df77e9416959ba9a57a12006dc83b7b163d
Joey Bruno <brunojoey12@gmail.com>
2020-05-18 22:23:45 -0500

    Double quotes to single quotes when working.

--- app.js
+++ app.js
@@ -1,104 +1,104 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-const render = require("./lib/htmlRenderer");
+const OUTPUT_DIR = path.resolve(__dirname, 'output');
+const outputPath = path.join(OUTPUT_DIR, 'team.html');
+const render = require('./lib/htmlRenderer');
 
 async function startApp() {
     const questions = [
         {
-            type: "confirm",
-            message: "Would you like to create a new employee?",
-            name: "newEmployee",
+            type: 'confirm',
+            message: 'Would you like to create a new employee?',
+            name: 'newEmployee',
         },
         {
-            type: "list",
-            message: "Which employee would we like to create?",
-            name: "employeeType",
+            type: 'list',
+            message: 'Which employee would we like to create?',
+            name: 'employeeType',
             choices: [
-                { name: "Manager", checked: false },
-                { name: "Engineer", checked: false },
-                { name: "Intern", checked: false }
+                { name: 'Manager', checked: false },
+                { name: 'Engineer', checked: false },
+                { name: 'Intern', checked: false }
             ]
         },
         {
-            type: "confirm",
-            message: "Would you like to create another employee?",
-            name: "anotherEmployee",
+            type: 'confirm',
+            message: 'Would you like to create another employee?',
+            name: 'anotherEmployee',
         }
     ];
 
     const engineerQuestions = [
         {
-            type: "input",
-            message: "Please enter your name.",
-            name: "name"
+            type: 'input',
+            message: 'Please enter your name.',
+            name: 'name'
         },
         {
-            type: "input",
-            message: "Please enter your work ID.",
-            name: "id"
+            type: 'input',
+            message: 'Please enter your work ID.',
+            name: 'id'
         },
         {
-            type: "input",
-            message: "Please enter your work email.",
-            name: "email"
+            type: 'input',
+            message: 'Please enter your work email.',
+            name: 'email'
         },
         {
-            type: "input",
-            message: "Please enter your work GitHub.",
-            name: "github"
+            type: 'input',
+            message: 'Please enter your work GitHub.',
+            name: 'github'
         }
     ];
 
     const managerQuestions = [
         {
-            type: "input",
-            message: "Please enter your name.",
-            name: "name"
+            type: 'input',
+            message: 'Please enter your name.',
+            name: 'name'
         },
         {
-            type: "input",
-            message: "Please enter your work ID.",
-            name: "id"
+            type: 'input',
+            message: 'Please enter your work ID.',
+            name: 'id'
         },
         {
-            type: "input",
-            message: "Please enter your work email.",
-            name: "email"
+            type: 'input',
+            message: 'Please enter your work email.',
+            name: 'email'
         },
         {
-            type: "input",
-            message: "Please enter your manager office number.",
-            name: "office"
+            type: 'input',
+            message: 'Please enter your manager office number.',
+            name: 'office'
         }
     ];
 
     const internQuestions = [
         {
-            type: "input",
-            message: "Please enter your name.",
-            name: "name"
+            type: 'input',
+            message: 'Please enter your name.',
+            name: 'name'
         },
         {
-            type: "input",
-            message: "Please enter your work ID.",
-            name: "id"
+            type: 'input',
+            message: 'Please enter your work ID.',
+            name: 'id'
         },
         {
-            type: "input",
-            message: "Please enter your work email.",
-            name: "email"
+            type: 'input',
+            message: 'Please enter your work email.',
+            name: 'email'
         },
         {
-            type: "input",
-            message: "Please enter the school you are attending.",
-            name: "school"
+            type: 'input',
+            message: 'Please enter the school you are attending.',
+            name: 'school'
         }
     ];
 
@@ -110,7 +110,7 @@ async function startApp() {
         if (newEmployee === true) {
             const type = await inquirer.prompt(questions[1]);
             const { employeeType } = type;
-            if (employeeType === "Manager") {
+            if (employeeType === 'Manager') {
                 const managerObject = await inquirer.prompt(managerQuestions);
                 const { name, id, email, office } = managerObject;
                 const newManager = new Manager(name, id, email, office);
@@ -119,7 +119,7 @@ async function startApp() {
                 const { anotherEmployee } = runAgain
                 exit = anotherEmployee;
             }
-            else if (employeeType === "Engineer") {
+            else if (employeeType === 'Engineer') {
                 const engineerObject = await inquirer.prompt(engineerQuestions);
                 const { name, id, email, github } = engineerObject;
                 const newEngineer = new Engineer(name, id, email, github);
@@ -128,7 +128,7 @@ async function startApp() {
                 const { anotherEmployee } = runAgain
                 exit = anotherEmployee;
             }
-            else if (employeeType === "Intern") {
+            else if (employeeType === 'Intern') {
                 const internObject = await inquirer.prompt(internQuestions);
                 const { name, id, email, school } = internObject;
                 const newIntern = new Intern(name, id, email, school);
@@ -143,9 +143,9 @@ async function startApp() {
     }
     while (exit === true);
     const teamHTML = await render(employees);
-    fs.writeFile(outputPath, teamHTML, "utf8", function (err) {
+    fs.writeFile(outputPath, teamHTML, 'utf8', function (err) {
         if (err) {
-            return console.log("Something went wrong.");
+            return console.log('Something went wrong.');
         };
     });
 };
